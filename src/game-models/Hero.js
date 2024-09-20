0a1cb5a6c2e0d447e20f1b79eb58eb761e6dff90
Dmitry <dmi.pol@icloud.com>
2024-09-20 13:10:38 +0300

    Function view

--- src/game-models/Hero.js
+++ src/game-models/Hero.js
@@ -1,19 +1,46 @@
 // Наш герой.
 
 class Hero {
-  constructor({ position } = {}) {
+  constructor() {
     this.skin = '🤠'; // можете использовать любые emoji '💃'
-    this.position = position || 0;
+    this.positionX = 0;
+    this.positionY = 2;
   }
 
   moveLeft() {
     // Идём влево.
-    this.position -= 1;
+    if (this.positionX === 0) {
+      this.positionX === 0;
+    } else {
+      this.positionX -= 1;
+    }
   }
 
   moveRight() {
     // Идём вправо.
-    this.position += 1;
+    if (this.positionY === 30) {
+      this.positionY === 30;
+    } else {
+      this.positionX += 1;
+    }
+  }
+
+  moveUp() {
+    // Идём вверх.
+    if (this.positionY === 0) {
+      this.positionY === 0;
+    } else {
+      this.positionY -= 1;
+    }
+  }
+
+  moveDown() {
+    // Идём вниз.
+    if (this.positionY === 4) {
+      this.positionY === 4;
+    } else {
+      this.positionY += 1;
+    }
   }
 
   attack() {
@@ -26,9 +53,6 @@ class Hero {
     console.log('YOU ARE DEAD!💀');
     process.exit();
   }
-  // --------------------------------------------------------------------------
-  
 }
-
 
 module.exports = Hero;
